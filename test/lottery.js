09a3d49e2b8d7eff98b207b2ef9784fa795ca0e7
PalakSDarji <palaksdarji@gmail.com>
2022-01-17 20:32:24 +0530

    few more testcases for lottery js

--- test/lottery.js
+++ test/lottery.js
@@ -33,13 +33,13 @@ contract('Lottery', (accounts) => {
 
   describe('enter the lottery', async ()=>{
     it('works fine with manager account', async ()=> {
-      await lottery.enter({from: accounts[0], value:1000000000000000000});
+      await lottery.enter({from: accounts[0], value: web3.utils.toWei('0.02','ether')});
       const firstPlayer = await lottery.players(0);
       assert.equal(firstPlayer,accounts[0]);
     });
 
     it('works fine with another account', async ()=> {
-      await lottery.enter({from: accounts[1], value:1000000000000000000});
+      await lottery.enter({from: accounts[1], value:web3.utils.toWei('0.02','ether')});
       const secondPlayer = await lottery.players(1);
       assert.equal(secondPlayer,accounts[1]);
     });
@@ -48,6 +48,42 @@ contract('Lottery', (accounts) => {
       const players = await lottery.getPlayers();
       assert.isAtLeast(players.length,1);
     });
+
+    it('fails if less ether is passed', async ()=> {
+      try{
+        await lottery.enter({from : accounts[2], value : 0});
+        assert(false);
+      } catch(err){
+        assert(err);
+      }
+    });
+
+    it('only manager can call pickWinner', async ()=> {
+      try{
+        await lottery.pickWinner({from : accounts[2]});
+        assert(false);
+      } catch(err) {
+        assert(err);
+      }
+    });
   });
 
+  describe('lottery winning', async ()=> {
+    it('winner gets the balance updated', async ()=> {
+      await lottery.enter({from: accounts[0], value: web3.utils.toWei('2','ether')});
+      const initialBal = await web3.eth.getBalance(accounts[0]);
+      await lottery.pickWinner();
+      const finalBal = await web3.eth.getBalance(accounts[0]);
+      const diff = finalBal - initialBal;
+      const ether = web3.utils.toWei('1.8','ether');
+      assert(diff > ether);
+    });
+
+    it('lottery balance gets to 0 once winner announce', async ()=> {
+      await lottery.enter({from: accounts[0], value: web3.utils.toWei('2','ether')});
+      await lottery.pickWinner();
+      const balance = await web3.eth.getBalance(lottery.address);
+      assert.equal(balance,0);
+    });
+  });
 });
