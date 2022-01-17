9cf61f6892df9cc3de4e748f25e2dab304d27d03
PalakSDarji <palaksdarji@gmail.com>
2022-01-17 16:20:46 +0530

    function modifiers added

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.11;
 contract Lottery{
 
   address public manager;
-  address[] public players;
+  address payable[] public players;
 
   constructor() {
     manager = msg.sender;
@@ -13,19 +13,20 @@ contract Lottery{
   function enter() public payable {
     require(msg.value > .01 ether);
 
-    players.push(msg.sender);
+    players.push(payable(msg.sender));
     emit EnteredLottery(msg.sender);
   }
 
   function random() private view returns(uint){
-    return uint(sha3(block.difficulty, now, players));
+    return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp,
+      players)));
   }
 
   function pickWinner() public restricted{
 
     uint index = random() % players.length;
-    players[index].transfer(this.balance);
-    players = new address[](0);
+    players[index].transfer(address(this).balance);
+    players = new address payable[](0);
   }
 
   modifier restricted(){
@@ -33,7 +34,7 @@ contract Lottery{
     _;
   }
 
-  function getPlayers() public view returns(address[]){
+  function getPlayers() public view returns(address payable[] memory){
     return players;
   }
 
--- test/lottery.js
+++ test/lottery.js
@@ -33,15 +33,31 @@ contract('Lottery', (accounts) => {
 
   describe('enter the lottery', async ()=>{
     it('works fine with manager account', async ()=> {
-      await lottery.enter({from: accounts[0]});
+      await lottery.enter({from: accounts[0], value:1000000000000000000});
       const firstPlayer = await lottery.players(0);
       assert.equal(firstPlayer,accounts[0]);
     });
 
     it('works fine with another account', async ()=> {
-      await lottery.enter({from: accounts[1]});
+      await lottery.enter({from: accounts[1], value:1000000000000000000});
       const secondPlayer = await lottery.players(1);
       assert.equal(secondPlayer,accounts[1]);
     });
+
+    it('can get players', async ()=> {
+      const players = await lottery.getPlayers();
+      assert.isAtLeast(players.length,1);
+    });
   });
+
+  describe('winner is', async ()=>{
+    it('the only member entered', async ()=>{
+      const balBeforeWinning = address(accounts[0]).balance;
+      const etherToEnterInLottery = 1000000000000000000;
+      await lottery.enter({from: account[0], value:etherToEnterInLottery});
+      await lottery.pickWinner();
+      assert.equal(address(account[0]).balance, balBeforeWinning + etherToEnterInLottery);
+    });
+  });
+
 });
